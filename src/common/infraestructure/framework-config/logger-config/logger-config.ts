805c5ef29b3287fe24ef015c714033bcb68c05ad
Daniel Bortot <daniel.bortot@gmail.com>
2025-05-14 12:03:32 -0400

    style: :art: agregando colores a los logs

--- src/common/infraestructure/framework-config/logger-config/logger-config.ts
+++ src/common/infraestructure/framework-config/logger-config/logger-config.ts
@@ -1,31 +1,65 @@
 import { ConsoleLogger } from '@nestjs/common';
 import * as fs from 'fs';
 
+enum LogLevel {
+    LOG = 'LOG',
+    ERROR = 'ERROR',
+    WARN = 'WARN',
+}
+
 export class FileLogger extends ConsoleLogger {
     private logStream = fs.createWriteStream('app.log', { flags: 'a' });
 
     constructor() {
         super();
         process.on('exit', () => this.logStream.end());
+
         process.on('SIGINT', () => {
             this.logStream.end();
             process.exit();
         });
+
+        process.on('uncaughtException', (err) => {
+            console.log('hola global')
+            this.error(`Uncaught Exception: ${err.message}`);
+            this.writeToFile(LogLevel.ERROR, `Uncaught Exception - ${err.stack}`);
+            process.exit(1);
+        });
+
+        process.on('unhandledRejection', (reason: any) => {
+            console.log('hola global')
+            if (reason instanceof AggregateError) {
+                
+                const errors = reason.errors.map((err: any) => {
+                    return err.stack as string || err.message as string || JSON.stringify(err);
+                });
+
+                this.error(`Unhandled Promise Rejection: ${errors.join(', ')}`);
+                this.writeToFile('ERROR', `Unhandled Promise Rejection: ${errors.join(', ')}`);
+            } else {
+                const errorMessage = reason instanceof Error ? reason.stack || reason.message : JSON.stringify(reason);
+                this.error(`Unhandled Promise Rejection: ${errorMessage}`);
+                this.writeToFile('ERROR', `Unhandled Promise Rejection - ${errorMessage}`);
+            }
+        });
+
+
+
     }
 
     log(message: string, context?: string) {
         super.log(message, context);
-        this.writeToFile('LOG', message, context);
+        this.writeToFile(LogLevel.LOG, message, context);
     }
 
     error(message: string, trace?: string, context?: string) {
         super.error(message, trace, context);
-        this.writeToFile('ERROR', message, context);
+        this.writeToFile(LogLevel.ERROR, message, context);
     }
 
     warn(message: string, context?: string) {
         super.warn(message, context);
-        this.writeToFile('WARN', message, context);
+        this.writeToFile(LogLevel.WARN, message, context);
     }
 
     private writeToFile(level: string, message: string, context?: string) {
@@ -39,7 +73,9 @@ export class FileLogger extends ConsoleLogger {
             hour12: false
         });
 
-        const logMessage = `[${formatter.format(new Date())}] ${level} [${context || 'NestJS'}]: ${message}\n`;
+        const color = level == LogLevel.LOG ? '\x1b[32m' : level == LogLevel.ERROR ? '\x1b[31m' : '\x1b[33m';
+
+        const logMessage = `[${formatter.format(new Date())}] ${color + level} [${context || 'NestJS'}]: ${message} \x1b[0m \n`;
         this.logStream.write(logMessage);
     }
 }
